913f74f37d7d2f7c54c71408c34f8eb25657fb21
Adam Voss <vossad01@gmail.com>
2011-08-20 22:41:33 -0500

    Added user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,11 @@ class User < ActiveRecord::Base
 
   before_save :encrypt_password
 
+  def feed
+    #
+    Micropost.where("user_id = ?", id)
+  end
+
   # Return true if the user's password matches the submitted password.
   def has_password?(submitted_password)
     encrypted_password == encrypt(submitted_password)
